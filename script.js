248b69054c92ee0e0a1b78de61a00c544038a7d4
LEGEND2310 <deepakkumarjha178531@gmail.com>
2021-11-20 19:37:47 +0530

    saves themes now

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+let theme = localStorage.getItem('theme');
+
+if(theme == null){
+    document.getElementById('theme-style').href = `light.css`;
+}else{
+    document.getElementById('theme-style').href = `${theme}.css`;
+}
+
 let themeDots = document.querySelectorAll('.theme-dot');
 
 for (var i = 0; i < themeDots.length; i++) {
     themeDots[i].addEventListener('click', function() {
         let mode = this.dataset.mode;
         document.getElementById('theme-style').href = `${mode}.css`;
+        localStorage.setItem('theme', mode);
     })
 }
